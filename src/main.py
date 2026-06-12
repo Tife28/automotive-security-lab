3718a50137c8de06aecae52243cbc2fdbccdf48f
Tife <61167252+Tife28@users.noreply.github.com>
2026-06-12 16:17:10 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
 import subprocess
+import os
 import time
 
 print("Starting Automotive Cybersecurity Lab...\n")
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+SIM_PATH = os.path.join(BASE_DIR, "can_simulator/can_simulator.py")
+ATTACKER_PATH = os.path.join(BASE_DIR, "can_simulator/attacker.py")
+
 # Start simulator
-sim = subprocess.Popen(["python", "src/can_simulator/can_simulator.py"])
+sim = subprocess.Popen(["python3", SIM_PATH])
 
 time.sleep(2)
 
 # Start attacker
-attacker = subprocess.Popen(["python", "src/can_simulator/attacker.py"])
+attacker = subprocess.Popen(["python3", ATTACKER_PATH])
 
 print("System running: Simulator + Attacker active")
 
