eb74e9a01cbf5f3d17137cf160ea0bf8f6ace8e8
Tife <61167252+Tife28@users.noreply.github.com>
2026-06-13 10:41:29 +0100

    The combined simulator

---
+++ src/can_simulator/main.py
@@ -0,0 +1,16 @@
+# run_demo.py
+
+import threading,time
+
+from ids import main as ids_main
+from can_simulator import main as sim_main
+from attacker import main as attacker_main
+from logs.bus_logger import main as logger_main
+
+threading.Thread(target=ids_main, daemon=True).start()
+threading.Thread(target=logger_main, daemon=True).start()
+threading.Thread(target=sim_main, daemon=True).start()
+threading.Thread(target=attacker_main, daemon=True).start()
+
+while True:
+    time.sleep(1)
