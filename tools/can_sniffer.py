7f1b9d2bb27db8167bad1db04a028c4d45919e91
Tife <61167252+Tife28@users.noreply.github.com>
2026-06-22 14:28:47 +0100

    Add a CAN Sniffer

--- tools/can_sniffer.py
+++ tools/can_sniffer.py
@@ -1,1 +1,16 @@
+import can
 
+print("Listening on vcan0...\n")
+
+bus = can.interface.Bus(
+    channel="vcan0",
+    interface="socketcan"
+)
+
+while True:
+    msg = bus.recv()
+
+    print(
+        f"ID=0x{msg.arbitration_id:X} "
+        f"DATA={msg.data.hex()}"
+    )
